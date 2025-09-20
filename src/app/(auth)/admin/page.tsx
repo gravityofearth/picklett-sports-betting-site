a20e2528c7592a3e2385cb432266cb0f2e26be43
toygr <milkyway464203@gmail.com>
2025-09-20 20:12:50 +0900

    fix: cron job status for auto resolution

--- src/app/(auth)/admin/page.tsx
+++ src/app/(auth)/admin/page.tsx
@@ -10,9 +10,10 @@ export default function Home() {
     const [tab, setTab] = useState<"line" | "redemption">("line")
     return (
         <div className="">
-            <div className="w-full flex justify-center items-center gap-2">
+            <div className="w-full flex flex-wrap justify-center items-center gap-2">
                 <img src="https://api.cron-job.org/jobs/6481181/74895d2b308e8173/status-3.svg" />
                 <img src="https://api.cron-job.org/jobs/6609976/211a340519d547e6/status-3.svg" />
+                <img src="https://api.cron-job.org/jobs/6614970/ef8881450c16905e/status-3.svg" />
             </div>
             <div className="w-full flex justify-center items-center gap-4">
                 <button onClick={() => setTab("line")} className={`${tab === "line" && "text-purple-600"} cursor-pointer`}>Line</button>
