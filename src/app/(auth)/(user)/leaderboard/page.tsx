ed9a3cbe93b228145872392f45bdde8c05b7e4a1
gravityofearth <gravityofearth9.8@gmail.com>
2025-10-13 13:36:39 +0900

    update: Winstreak placeholder

--- src/app/(auth)/(user)/leaderboard/page.tsx
+++ src/app/(auth)/(user)/leaderboard/page.tsx
@@ -16,7 +16,7 @@ export default async function WinstreakPage() {
             <svg className="w-6 h-6 shrink-0"><use href="#svg-crown" /></svg>
             <div className="flex flex-col gap-2">
               <p className="text-[#FFB900]">Leaderboard & Rewards</p>
-              <p className="text-[#FEF3C6CC]">Stay on top of the action with our live leaderboard, showcasing players on winning streaks of 2 or more. Aim higher to unlock instant exclusive bonuses straight to your balance from $50 at 5 wins, to $250 at 10 wins. The more you win, the greater the rewards. (You dont need to bet on every game, just choose the ones you have confidence in!)</p>
+              <p className="text-[#FEF3C6CC]">Stay on top of the action with our live leaderboard, showcasing players on winning streaks of 2 or more. Aim higher to unlock instant exclusive bonuses straight to your balance from $50 at 5 wins, to $200 at 10 wins. The more you win, the greater the rewards. (You dont need to bet on every game, just choose the ones you have confidence in!)</p>
             </div>
           </div>
           <div className="w-full flex flex-col gap-2 pt-2 bg-linear-0 from-[#1018284D] to-[#1E293933] border border-[#36415380] rounded-2xl">
@@ -52,7 +52,7 @@ export default async function WinstreakPage() {
                     </td>
                     <td className="pl-1 py-2 pr-5">
                       <div className="flex gap-2 items-center">
-                        <span className="text-[18px] text-[#01A3DB]">$100</span>
+                        <span className="text-[18px] text-[#01A3DB]">$70</span>
                         <div className="text-xs text-[#99A1AF] px-2 py-1 rounded-full bg-[#1E2939]">Silver</div>
                       </div>
                     </td>
@@ -64,7 +64,7 @@ export default async function WinstreakPage() {
                     </td>
                     <td className="pl-1 py-2 pr-5">
                       <div className="flex gap-2 items-center">
-                        <span className="text-[18px] text-[#01A3DB]">$250</span>
+                        <span className="text-[18px] text-[#01A3DB]">$200</span>
                         <div className="text-xs text-[#99A1AF] px-2 py-1 rounded-full bg-[#1E2939]">Gold</div>
                       </div>
                     </td>
