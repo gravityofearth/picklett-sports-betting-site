7cfd37603b5e867a257e1a46061fe8f237bb45a5
gravityofearth <gravityofearth9.8@gmail.com>
2026-01-21 16:51:06 +0900

    update: new cron-job for auto resolve

--- src/app/(auth)/admin/(admin)/layout.tsx
+++ src/app/(auth)/admin/(admin)/layout.tsx
@@ -12,7 +12,7 @@ export default function AuthLayout({
         <div className="w-full flex flex-col gap-2">
             <div className="w-full flex flex-wrap justify-center items-center gap-2">
                 <img src="https://api.cron-job.org/jobs/6481181/74895d2b308e8173/status-3.svg" />
-                <img src="https://api.cron-job.org/jobs/6614970/ef8881450c16905e/status-3.svg" />
+                <img src="https://api.cron-job.org/jobs/7179928/b2cf11b48f3866c7/status-3.svg" />
             </div>
             <div className="flex flex-wrap justify-center items-center gap-x-8 gap-y-2 font-semibold text-[#D1D5DC] py-2">
                 <Link href="/admin/redemption" className={`${pathname === "/admin/redemption" ? "text-[#00BFFF]" : ""}`}>Redemption</Link>
